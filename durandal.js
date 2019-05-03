a0320fee135952c73342ebccbebf1d1ad832da77
Ange Pagel <inkandev@gmail.com>
2019-05-03 12:30:33 +0200

    Command handling

--- durandal.js
+++ durandal.js
@@ -8,10 +8,17 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', message => {
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+  if (command === 'ping') {
+    message.reply('Pong!');
   }
+
 });
 
 client.login(token);
