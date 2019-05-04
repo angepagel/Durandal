06bbecee5ee1ecb5b0e3ac06b80938f22abe9bff
Ange Pagel <inkandev@gmail.com>
2019-05-04 16:49:10 +0200

    Dynamic command handling

--- durandal.js
+++ durandal.js
@@ -1,8 +1,16 @@
-
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const { token, prefix } = require('./config.json');
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -16,7 +24,8 @@ client.on('message', message => {
 	const command = args.shift().toLowerCase();
 
   if (command === 'ping') {
-    message.reply('Pong!');
+    client.commands.get('ping').execute(message, args);
+  }
   }
 
 });
